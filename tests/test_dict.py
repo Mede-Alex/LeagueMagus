127fa05722b4d1c9a69d97f354898056052d47a6
Mede <you@example.com>
2023-07-23 19:19:34 +0300

    Modified functionality

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -15,16 +15,16 @@ class DictTest(unittest.TestCase):
         """Check for Null values"""
         bad_shit_happened = False
             
-        for key, values in self.dict_obj.items():
+        for _, values in self.dict_obj.items():
             if None in values:
                 bad_shit_happened = True
-        self.assertEqual(bad_shit_happened, False)
+        self.assertIs(bad_shit_happened, False)
 
     def test_3(self):
         """Datatypes testing"""
         bad_shit_happened = False
         dtypes_dict = {int : 43, bool: 3} #should be illegal
-        l = [type(values[0]) for key, values in self.dict_obj.items()]
+        l = [type(values[0]) for _, values in self.dict_obj.items()]
         freq_dict = {}
         for el in l:
             if el in freq_dict:
@@ -33,4 +33,4 @@ class DictTest(unittest.TestCase):
                 freq_dict[el] = 1
         if freq_dict != dtypes_dict:
             bad_shit_happened = True
-        self.assertEqual(bad_shit_happened, False)
+        self.assertIs(bad_shit_happened, False)
