be63a062e8f89e785399810e3a4dee78e72b07ae
Mede <you@example.com>
2023-07-23 17:48:58 +0300

    Added some tests

---
+++ tests/test_dict.py
@@ -0,0 +1,36 @@
+import unittest
+import pickle
+
+class DictTest(unittest.TestCase):
+    
+    PATH = './data/dict.pkl'    
+    with open(PATH, 'rb') as f:
+            dict_obj = pickle.load(f)
+    
+    def test_1(self):
+        """Check base test"""
+        self.assertTrue(issubclass(DictTest, unittest.TestCase))
+    
+    def test_2(self):
+        """Check for Null values"""
+        bad_shit_happened = False
+            
+        for key, values in self.dict_obj.items():
+            if None in values:
+                bad_shit_happened = True
+        self.assertEqual(bad_shit_happened, False)
+
+    def test_3(self):
+        """Datatypes testing"""
+        bad_shit_happened = False
+        dtypes_dict = {int : 43, bool: 3} #should be illegal
+        l = [type(values[0]) for key, values in self.dict_obj.items()]
+        freq_dict = {}
+        for el in l:
+            if el in freq_dict:
+                freq_dict[el] += 1
+            else:
+                freq_dict[el] = 1
+        if freq_dict != dtypes_dict:
+            bad_shit_happened = True
+        self.assertEqual(bad_shit_happened, False)
